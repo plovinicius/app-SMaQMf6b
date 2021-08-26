9cf75f1f927ea7fbc919bb4c351ac472331b4639
Paulo Vinicius <ploviniciusplo@gmail.com>
2021-08-26 02:10:43 -0300

    feat(request): cria request para cadastro de produto

---
+++ app/Http/Requests/Product/ProductStoreRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'sku' => ['required', 'max:255', 'unique:products'],
+            'stock' => ['required', 'integer', 'min:0']
+        ];
+    }
+}
