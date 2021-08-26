bbc841289e27c9f2c940085a18925f6302258753
Paulo Vinicius <ploviniciusplo@gmail.com>
2021-08-26 02:31:15 -0300

    feat(product): cria rota e controller para editar stock

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Actions\Product\ProductCreateAction;
+use App\Actions\Product\ProductStockUpdateAction;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Product\ProductStockUpdateRequest;
 use App\Http\Requests\Product\ProductStoreRequest;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
@@ -21,7 +23,10 @@ class ProductController extends Controller
     /**
      * @throws Exception
      */
-    public function store(ProductStoreRequest $request, ProductCreateAction $storeAction): ProductResource
+    public function store(
+        ProductStoreRequest $request,
+        ProductCreateAction $storeAction
+    ): ProductResource
     {
         try {
             $product = $storeAction->execute($request->validated());
@@ -29,6 +34,24 @@ class ProductController extends Controller
             return new ProductResource($product);
         } catch (Exception $ex) {
             throw new Exception('Can\'t create a new product, please, try again later.');
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function update(
+        Product $product,
+        ProductStockUpdateRequest $request,
+        ProductStockUpdateAction $updateAction
+    ): ProductResource
+    {
+        try {
+            $product = $updateAction->execute($product, $request->validated());
+
+            return new ProductResource($product);
+        } catch (Exception $ex) {
+            throw new Exception('Can\'t update product stock, please, try again later.');
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,6 @@ Route::name('api.')->group(function() {
     Route::prefix('products')->name('products.')->group(function() {
         Route::get('/', [ProductController::class, 'index'])->name('index');
         Route::post('/', [ProductController::class, 'store'])->name('store');
+        Route::patch('/{product}/edit', [ProductController::class, 'update'])->name('update');
     });
 });
